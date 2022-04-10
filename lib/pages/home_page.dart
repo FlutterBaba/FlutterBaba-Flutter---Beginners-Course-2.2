92bb8915fda7e6be1b8ccd83d00147bf5ab85e56
Flutter  Baba <flutterbaba@gmail.com>
2022-04-10 23:49:33 +0500

    first commit

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String name = "Yaqoob";
+
+  void _onPressed(name) {
+    setState(() {
+      this.name = name;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        actions: const [
+          Icon(Icons.show_chart),
+        ],
+        centerTitle: true,
+        title: const Text("My App"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(name),
+            ElevatedButton(
+              onPressed: () => _onPressed("Aqeel Baloch"),
+              style: ElevatedButton.styleFrom(
+                primary: Colors.red,
+              ),
+              child: const Text(
+                "Click me ",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
